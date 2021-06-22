5f3c98fc41804a95545761fdbd57a4454a5a80a1
Shreyaswini-M-R <74600067+Shreyaswini-M-R@users.noreply.github.com>
2021-06-22 22:44:20 +0530

    Add files via upload

---
+++ PositiveNumbers.py
@@ -0,0 +1,10 @@
+list1 = [12, -7, 5, 64, -14]
+list2 = [12, 14, -95, 3]
+print("List1 :", end=" ")
+for num in list1:
+    if num >= 0:
+       print(num, end=" ")
+print("\nList2 :", end=" ")
+for num in list2:
+    if num >= 0:
+       print(num, end=" ")
